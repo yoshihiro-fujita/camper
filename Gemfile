2384ae15f0d330d5ec58425d298295471f396c73
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-09-11 11:07:23 +0900

    rspecセットアップ完了

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-commands-rspec'
 
 end
 
@@ -60,6 +61,7 @@ group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem 'rspec-rails', '~> 3.6.0'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
 end
--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,17 @@ module Camper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.test framework :rspec,
+      fixtures:false,
+      view_specs: false,
+      helper_specs:false,
+      routing_specs:false
+    end
   end
 end
